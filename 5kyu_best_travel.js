aad6958486428c4062414e59d8bdb6b34c6bee5f
Radu Constantin <raduadrian.constantin94@gmail.com>
2021-07-22 11:06:09 +0300

    Started best_travel

---
+++ 5kyu_best_travel.js
@@ -0,0 +1,35 @@
+function chooseBestSum(t, k, ls) {
+  // your code
+}
+
+function getPermutations
+
+/*
+Input:
+- t => maximum sum of distances (number);
+- k => number of towns to visit (number);
+- ls => list of distances (array).
+
+Output:
+- Greatest possible sum of k towns within list(ls) below t (maximum sum);
+
+Algorithm and Data Structure:
+Array
+
+1. Get all possible combinations of towns in order;
+
+2. Filter and keep only the combinations that are under t;
+3. Select the combination that has the highest sum;
+
+Test Case:
+t = 174;
+k = 3;
+ls = [50, 55, 57, 58, 60]; length = 5;
+
+Towns = x, y, z;
+xIndex = 0;
+yIndex = 1;
+zIndex = 2;
+
+
+*/
